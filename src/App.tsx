cb5d88796588dc5d95094ad1aa8e31ce73e3466b
Drew <Jennings>
2022-09-07 08:24:43 +0100

    add basic navigation

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,47 @@
 import React, { useState, useEffect } from "react";
 import { Box, Typography } from "@mui/material";
 import axios from "axios";
+import { Navigation } from "./components/Navigation/Navigation";
 
 axios.defaults.headers.common["Access-Control-Allow-Origin"] = "*";
 axios.defaults.headers.common["Access-Control-Allow-Methods"] =
   "GET,PUT,POST,DELETE,PATCH,OPTIONS";
 
-type BasicInfo = {
-  name: string;
-  level: number;
-  class: string;
-};
+// type BasicInfo = {
+//   name: string;
+//   level: number;
+//   class: string;
+// };
 
 function App() {
-  const [basicInfo, setBasicInfo] = useState<BasicInfo>();
+  // const [basicInfo, setBasicInfo] = useState<BasicInfo>();
+  const [selectedTab, setSelectedTab] = useState("");
 
-  useEffect(() => {
-    axios
-      .get("http://localhost:3001/basicInfo")
-      .then((res: { data: BasicInfo }) => setBasicInfo(res.data))
-      .catch((error) => console.log(error));
-  }, []);
+  // useEffect(() => {
+  //   axios
+  //     .get("http://localhost:3001/basicInfo")
+  //     .then((res: { data: BasicInfo }) => setBasicInfo(res.data))
+  //     .catch((error) => console.log(error));
+  // }, []);
 
   return (
-    <Box>
-      <Typography>Name: {basicInfo?.name}</Typography>
-      <Typography>Level: {basicInfo?.level}</Typography>
-      <Typography>Class: {basicInfo?.class}</Typography>
-    </Box>
+    <>
+      <Navigation selectedTab={(name) => setSelectedTab(name)} />
+      {selectedTab === "Character" && <>Char!</>}
+      {selectedTab === "Skills" && <>Skills!</>}
+      {selectedTab === "Spells" && <>Spells!</>}
+      {selectedTab === "Abilities" && <>Abilities!</>}
+      {selectedTab === "Inventory" && <>Inventory!</>}
+    </>
   );
 }
 
 export default App;
+
+// seperate into sections:
+
+// Character Info: Name, Race, Class Stats, Saving throws,
+// Skills
+// Spells
+// Abilites - Attacks - Feats - Specials
+// Inventory - Money
---
+++ src/components/Navigation/Navigation.tsx
@@ -0,0 +1,27 @@
+import React, { ReactElement, useState } from "react";
+import { Tabs, Tab } from "@mui/material";
+
+type NavigationProps = {
+  selectedTab: (name: string) => void;
+};
+
+export function Navigation(props: NavigationProps): ReactElement {
+  const tabList = ["Character", "Skills", "Spells", "Abilities", "Inventory"];
+  const [currentTab, setCurrentTab] = useState("Character");
+
+  return (
+    <Tabs value={currentTab}>
+      {tabList.map((tab, i) => (
+        <Tab
+          key={i}
+          value={tab}
+          label={tab}
+          onClick={() => {
+            setCurrentTab(tab);
+            props.selectedTab(tab);
+          }}
+        />
+      ))}
+    </Tabs>
+  );
+}
