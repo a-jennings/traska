88cf078e2555b5d55ac5b5037803bd522c942fab
Drew Jennings <andrew_jennings@ymail.com>
2022-09-07 19:13:56 +0100

    prepare for add/edit dialog

--- src/pages/Skills/Skills.tsx
+++ src/pages/Skills/Skills.tsx
@@ -1,5 +1,13 @@
 import React, { ReactElement, useState, useEffect, Fragment } from "react";
-import { Box, Typography, Grid, Divider, IconButton, Fab } from "@mui/material";
+import {
+  Box,
+  Typography,
+  Grid,
+  Divider,
+  IconButton,
+  Fab,
+  Dialog,
+} from "@mui/material";
 import axios from "axios";
 import { Skill } from "../../types";
 import EditIcon from "@mui/icons-material/Edit";
@@ -11,6 +19,15 @@ axios.defaults.headers.common["Access-Control-Allow-Methods"] =
 
 export function Skills(): ReactElement {
   const [data, setData] = useState<Array<Skill>>();
+  const [dialogOpen, setOpenDialog] = useState(false);
+
+  const handleDialogOpen = () => {
+    setOpenDialog(true);
+  };
+
+  const handleDialogClose = () => {
+    setOpenDialog(false);
+  };
 
   useEffect(() => {
     axios
@@ -20,77 +37,90 @@ export function Skills(): ReactElement {
   }, []);
 
   return (
-    <Box py={3} px={8}>
-      <Grid container>
-        <Grid xs={6} item>
-          <Typography>Skill Name</Typography>
+    <>
+      <Box py={3} px={8}>
+        <Grid container>
+          <Grid xs={6} item>
+            <Typography>Skill Name</Typography>
+          </Grid>
+          <Grid xs={1} item textAlign="center">
+            <Typography>Key Ability</Typography>
+          </Grid>
+          <Grid xs={1} item textAlign="center">
+            <Typography>Skill Modifier</Typography>
+          </Grid>
+          <Grid xs={1} item textAlign="center">
+            <Typography>Ability Modifier</Typography>
+          </Grid>
+          <Grid xs={1} item textAlign="center">
+            <Typography>Ranks</Typography>
+          </Grid>
+          <Grid xs={1} item textAlign="center">
+            <Typography>Misc Modifier</Typography>
+          </Grid>
+          <Grid xs={1} item textAlign="center">
+            <Typography>Edit</Typography>
+          </Grid>
         </Grid>
-        <Grid xs={1} item textAlign="center">
-          <Typography>Key Ability</Typography>
-        </Grid>
-        <Grid xs={1} item textAlign="center">
-          <Typography>Skill Modifier</Typography>
-        </Grid>
-        <Grid xs={1} item textAlign="center">
-          <Typography>Ability Modifier</Typography>
-        </Grid>
-        <Grid xs={1} item textAlign="center">
-          <Typography>Ranks</Typography>
-        </Grid>
-        <Grid xs={1} item textAlign="center">
-          <Typography>Misc Modifier</Typography>
-        </Grid>
-        <Grid xs={1} item textAlign="center">
-          <Typography>Edit</Typography>
-        </Grid>
-      </Grid>
-      <Box mt={1}>
-        <Divider />
+        <Box mt={1}>
+          <Divider />
+        </Box>
+
+        {data?.map((skill, i) => (
+          <Fragment key={i}>
+            <Grid container>
+              <Grid xs={6} item>
+                <Typography>{skill?.name}</Typography>
+              </Grid>
+              <Grid xs={1} item textAlign="center">
+                <Typography>{skill?.keyAbility}</Typography>
+              </Grid>
+              <Grid xs={1} item textAlign="center">
+                <Typography>{skill?.skillModifier}</Typography>
+              </Grid>
+              <Grid xs={1} item textAlign="center">
+                <Typography sx={{ opacity: 0.4 }}>
+                  {skill?.abilityModifier}
+                </Typography>
+              </Grid>
+              <Grid xs={1} item textAlign="center">
+                <Typography sx={{ opacity: 0.4 }}>{skill?.ranks}</Typography>
+              </Grid>
+              <Grid xs={1} item textAlign="center">
+                <Typography sx={{ opacity: 0.4 }}>
+                  {skill?.miscModifier}
+                </Typography>
+              </Grid>
+              <Grid
+                xs={1}
+                item
+                display="flex"
+                justifyContent="center"
+                alignItems="center"
+              >
+                <IconButton sx={{ height: "20px", width: "20px" }}>
+                  <EditIcon sx={{ height: "20px", width: "20px" }} />
+                </IconButton>
+              </Grid>
+            </Grid>
+            <Divider />
+          </Fragment>
+        ))}
+        <Fab
+          sx={{ position: "absolute", bottom: 30, right: 30 }}
+          color="primary"
+          onClick={handleDialogOpen}
+        >
+          <AddIcon />
+        </Fab>
       </Box>
 
-      {data?.map((skill, i) => (
-        <Fragment key={i}>
-          <Grid container>
-            <Grid xs={6} item>
-              <Typography>{skill?.name}</Typography>
-            </Grid>
-            <Grid xs={1} item textAlign="center">
-              <Typography>{skill?.keyAbility}</Typography>
-            </Grid>
-            <Grid xs={1} item textAlign="center">
-              <Typography>{skill?.skillModifier}</Typography>
-            </Grid>
-            <Grid xs={1} item textAlign="center">
-              <Typography sx={{ opacity: 0.4 }}>
-                {skill?.abilityModifier}
-              </Typography>
-            </Grid>
-            <Grid xs={1} item textAlign="center">
-              <Typography sx={{ opacity: 0.4 }}>{skill?.ranks}</Typography>
-            </Grid>
-            <Grid xs={1} item textAlign="center">
-              <Typography sx={{ opacity: 0.4 }}>
-                {skill?.miscModifier}
-              </Typography>
-            </Grid>
-            <Grid
-              xs={1}
-              item
-              display="flex"
-              justifyContent="center"
-              alignItems="center"
-            >
-              <IconButton sx={{ height: "20px", width: "20px" }}>
-                <EditIcon sx={{ height: "20px", width: "20px" }} />
-              </IconButton>
-            </Grid>
-          </Grid>
-          <Divider />
-        </Fragment>
-      ))}
-      <Fab sx={{ position: "absolute", bottom: 30, right: 30 }} color="primary">
-        <AddIcon />
-      </Fab>
-    </Box>
+      <Dialog
+        maxWidth="sm"
+        fullWidth
+        open={dialogOpen}
+        onClose={handleDialogClose}
+      ></Dialog>
+    </>
   );
 }
