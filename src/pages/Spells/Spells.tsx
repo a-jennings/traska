d82d1755c713a8f8842fe9c5763ff3029e7e67c3
Drew Jennings <andrew_jennings@ymail.com>
2022-09-11 19:31:01 +0100

    start making adjustements to spells

--- src/pages/Spells/Spells.tsx
+++ src/pages/Spells/Spells.tsx
@@ -3,29 +3,39 @@ import { SpellHeader } from "./SpellHeader/SpellHeader";
 import { SpellMenu } from "./SpellMenu/SpellMenu";
 import axios from "axios";
 import { SpellSlot } from "../../types";
+import { Tabs, Tab, useTheme, Box } from "@mui/material";
 
 axios.defaults.headers.common["Access-Control-Allow-Origin"] = "*";
 axios.defaults.headers.common["Access-Control-Allow-Methods"] =
   "GET,PUT,POST,DELETE,PATCH,OPTIONS";
 
 export function Spells(): ReactElement {
-  const [data, setData] = useState<Array<SpellSlot>>();
+  const [activeTab, setActiveTab] = useState(0);
+  const theme = useTheme();
 
-  useEffect(() => {
-    axios
-      .get("http://localhost:3001/spellSlots")
-      .then((res: { data: Array<SpellSlot> }) => setData(res.data))
-      .catch((error) => console.log(error));
-  }, []);
+  const tabStyles = {
+    backgroundColor: theme.palette.grey[300],
+  };
 
-  if (!data) {
-    return <></>;
-  }
+  const handleChange = (event: React.SyntheticEvent, newValue: number) => {
+    setActiveTab(newValue);
+  };
 
   return (
     <>
-      <SpellHeader data={data} setData={setData} />
-      <SpellMenu data={data} setData={setData} />
+      <Box mt={2} px={4}>
+        <Tabs
+          variant="fullWidth"
+          onChange={handleChange}
+          value={activeTab}
+          indicatorColor="secondary"
+          textColor="secondary"
+        >
+          <Tab label="One" sx={tabStyles} />
+          <Tab label="Two" sx={tabStyles} />
+          <Tab label="Three" sx={tabStyles} />
+        </Tabs>
+      </Box>
     </>
   );
 }
